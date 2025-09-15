9ac3de637198e7460b019e07c24d90686bced4bb
enescnby <enes1canbay@gmail.com>
2025-09-15 18:03:34 +0300

    feature(news-api): API Docs integrated

--- news-api/src/main/java/org/enes/newsapi/dto/NewsDto.java
+++ news-api/src/main/java/org/enes/newsapi/dto/NewsDto.java
@@ -1,13 +1,24 @@
 package org.enes.newsapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record NewsDto(
+
+        @Schema(description = "News ID")
         String id,
+        @Schema(description = "News title")
         String title,
+        @Schema(description = "News link")
         String link,
+        @Schema(description = "News description")
         String description,
+        @Schema(description = "News publication date")
         String pubDate,
+        @Schema(description = "News source")
         String source,
+        @Schema(description = "News thumbnail URL")
         String thumbnailUrl,
+        @Schema(description = "News content URL")
         String contentUrl
 ) {
 }
